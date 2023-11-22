de8a34c0c26c63aac9693dde7242bfab475bad26
ubilby <ubilby@gmail.com>
2023-11-22 18:40:29 +0300

    add book app with model

---
+++ backend/books/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=80)
+    author = models.CharField(verbose_name="Автор", max_length=80)
+    year = models.PositiveSmallIntegerField(verbose_name="Год издания")
+    author = models.CharField(verbose_name="ISBN", max_length=13)

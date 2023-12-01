e02e924545e91922462d02d3d8f022e8f6082ecc
ubilby <ubilby@gmail.com>
2023-12-01 03:11:32 +0300

    add validate book's data

--- backend/books/serializers.py
+++ backend/books/serializers.py
@@ -7,3 +7,17 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'author', 'year', 'isbn')
+
+    def validate(self, data):
+        title = data.get('title')
+        author = data.get('author')
+        year = data.get('year')
+        if Book.objects.filter(
+            title=title,
+            author=author,
+            year=year
+        ).exists():
+            raise serializers.ValidationError(
+                "Такая книга уже существует!"
+            )
+        return data
